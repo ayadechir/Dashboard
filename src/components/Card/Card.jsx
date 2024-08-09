d40b9729982bcdb58279f9ad45f5b04a95d11758
thenewinfo <ayadechir2003@gmail.com>
2024-08-09 16:17:29 +0100

    9

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,34 +1,46 @@
-import React, { useState } from 'react'
-import { AnimateSharedLayout } from 'framer-motion'
-const Card = (props) => {
-  const [expanded, setExpanded]=useState(false)
-  return (
-    <AnimateSharedLayout>
-      {
-      expanded?(
-        'Expanded'
-      ):
-      <CompactCard param={props}/>
-    }
-    </AnimateSharedLayout>
-  )
-}
+import React, { useState } from 'react';
+import { LayoutGroup } from 'framer-motion'; 
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import	'./Card.css';
 
-//CompactCard 
-function CompactCard ({param}){
+const Card = (props) => {
+  const [expanded, setExpanded] = useState(false);
+  
+  return (
+    <LayoutGroup>
+      {expanded ? (
+        'Expanded'
+      ) : (
+        <CompactCard param={props} />
+      )}
+    </LayoutGroup>
+  );
+};
+
+// CompactCard component
+function CompactCard({ param }) {
   const Png = param.png;
-  return(
-    <div className="CompactCard">
+  
+  return (
+    <div className="CompactCard"
+      style={{
+        background : param.color.backGround,
+        boxShadow : param.color.boxShadow,
+      }}>
       <div className="radialBar">
-        Chart
+        <CircularProgressbar
+        value={param.barValue}
+        text={`${param.barValue}%`}
+        />
       </div>
       <div className="detail">
-        <Png/>
+        <Png />
         <span>${param.value}</span>
         <span>Last 24</span>
       </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
